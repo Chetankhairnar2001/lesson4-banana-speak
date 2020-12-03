60784ce9260229f5d3ff0f83293612c6a41480b3
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-03 23:02:18 +0530

    docs: added wire connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var inputbtn=document.querySelector("#inputbtn");
-console.log("clci wmeme");
+var inputtxt=document.querySelector("#inputtxt");
+var outputtxt=document.querySelector("#outputtxt");
+
+console.log(outputtxt);
 inputbtn.addEventListener("click",function click(){
     console.log("u click me");
+    console.log("input",inputtxt.value);
 });
