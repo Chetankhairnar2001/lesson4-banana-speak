ba81f33a23b9767641a51731abb08a10303c801a
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-03 23:18:12 +0530

    input output processing

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var inputbtn=document.querySelector("#inputbtn");
 var inputtxt=document.querySelector("#inputtxt");
 var outputtxt=document.querySelector("#outputtxt");
 
-console.log(outputtxt);
-inputbtn.addEventListener("click",function click(){
-    console.log("u click me");
-    console.log("input",inputtxt.value);
-});
+
+function translate(){
+  outputtxt.innerText="click "+inputtxt.value;
+}
+inputbtn.addEventListener("click",translate);
