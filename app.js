b9fa1ac90b525b602528c23e33ce46aaa59d7f85
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-04 13:04:45 +0530

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var inputtxt=document.querySelector("#inputtxt");
 var outputtxt=document.querySelector("#outputtxt");
 
 // var serverurlold="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverurl="https://api.funtranslations.com/translate/minion.json"
+var serverurl="https://api.funtranslations.com/translate/minion.json";
 
 function geturltranslate(text){
   return serverurl+ "?" +"text="+ text;
@@ -12,15 +12,14 @@ function geturltranslate(text){
 // fetch(geturltranslate(inputtxt)).then(rse=>rse.json()).then(ro=>console.log(ro.contents.translated));
 
 function translate(){
-    var inputtext=inputtxt.value; //input
+    var inputtext=inputtxt.value;
 
 //calling server for processing
     fetch(geturltranslate(inputtext))
     .then(response =>response.json())
-    .then(json =>{
+    .then(json => {
       var translatedtext=json.contents.translated;
       outputtxt.innerText=translatedtext;   //output
-    });
-  
-}
+    }) 
+};
 inputbtn.addEventListener("click",translate);
