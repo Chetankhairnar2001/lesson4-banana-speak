11b6a3b394d4bef1abca5b59fafadc1b61bd8629
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-04 12:41:28 +0530

    wired minion api and processed output

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ var inputbtn=document.querySelector("#inputbtn");
 var inputtxt=document.querySelector("#inputtxt");
 var outputtxt=document.querySelector("#outputtxt");
 
-var serverurl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverurlold="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverurl="https://api.funtranslations.com/translate/minion.json"
 
 function geturltranslate(text){
   return serverurl+ "?" +"text="+ text;
 }
+//  just for practise
+// fetch(geturltranslate(inputtxt)).then(rse=>rse.json()).then(ro=>console.log(ro.contents.translated));
 
 function translate(){
-  var inputtext=inputtxt.value;
+    var inputtext=inputtxt.value; //input
+
 //calling server for processing
- fetch(geturltranslate(inputtext))
- .then(response =>response.json())
- .then(json =>console.log(json.contents.text));
+    fetch(geturltranslate(inputtext))
+    .then(response =>response.json())
+    .then(json =>{
+      var translatedtext=json.contents.translated;
+      outputtxt.innerText=translatedtext;   //output
+    });
   
 }
 inputbtn.addEventListener("click",translate);
