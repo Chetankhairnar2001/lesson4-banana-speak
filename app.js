84b2040a2af62082da41ef2b23b1b349d92ab7af
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-04 12:05:22 +0530

    conected the input to api server

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ var inputbtn=document.querySelector("#inputbtn");
 var inputtxt=document.querySelector("#inputtxt");
 var outputtxt=document.querySelector("#outputtxt");
 
+var serverurl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function geturltranslate(text){
+  return serverurl+ "?" +"text="+ text;
+}
 
 function translate(){
-  outputtxt.innerText="click "+inputtxt.value;
+  var inputtext=inputtxt.value;
+//calling server for processing
+ fetch(geturltranslate(inputtext))
+ .then(response =>response.json())
+ .then(json =>console.log(json.contents.text));
+  
 }
 inputbtn.addEventListener("click",translate);
